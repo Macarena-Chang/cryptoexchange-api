3c4d535afd5cdeb276ee96d59344459d000b0493
Macarena Chang <changmacarena@gmail.com>
2024-10-08 00:14:36 -0300

    project setup

---
+++ cryptoexchange-api/src/main/java/com/example/cryptoexchange_api/CryptoexchangeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoexchange_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoexchangeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoexchangeApiApplication.class, args);
+	}
+
+}
---
+++ cryptoexchange-api/src/test/java/com/example/cryptoexchange_api/CryptoexchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoexchange_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoexchangeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
